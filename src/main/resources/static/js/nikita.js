f78b089757793a439a77c7ce27a5b103aa009abc
Dmytro <dmitry.b@scopicsoftware.com>
2021-10-20 21:09:14 +0300

    gradle

--- src/main/resources/static/js/nikita.js
+++ src/main/resources/static/js/nikita.js
@@ -1,5 +1,11 @@
-function changeColor() {
-    var p2 = 'ddfdfdfdf';
-    document.getElementById('p2').style.color = 'green';
-}
-changeColor();
+var menuToggle = document.querySelector("#menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+var toggledMenu = menuToggle.addEventListener("click", function(){
+    // forEach is not supported in IE11
+    // activeElements.forEach(function(e){
+    //     e.classList.toggle("active");
+    // });
+    for(var activated = 0; activated < activeElements.length; activated++){
+        activeElements[activated].classList.toggle("active");
+    }
+})
